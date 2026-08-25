94fa93d1b3e88759ac7625e13f6bc28955e2bc99
MuhammadAhsanZahoor <109317164+codewithahsanzahoor@users.noreply.github.com>
2026-08-26 02:21:19 +0500

    some bug fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  // Is line ko add karein taake Serveo ka link allow ho sake
+  allowedDevOrigins: ["*.serveousercontent.com"],
+  allowedDevOrigins: ["*.shares.zrok.io"],
 };
 
 export default nextConfig;
